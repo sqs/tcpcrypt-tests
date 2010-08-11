a078975d312170358960e4de8f8552ab6a44510d
Quinn Slack <sqs@cs.stanford.edu>
2010-08-10 22:29:43 -0700

    no more autotools

--- ubuntu-10-04-i386-install.sh
+++ ubuntu-10-04-i386-install.sh
@@ -2,7 +2,7 @@ apt-get install -y \
     iptables libnfnetlink-dev libnetfilter-queue-dev libcap-dev libssl-dev
 
 cd tcpcrypt/user
-./configure && make
+make
 
 iptables -I INPUT -p tcp -j NFQUEUE --queue-num 666
 iptables -I OUTPUT -p tcp -j NFQUEUE --queue-num 666
