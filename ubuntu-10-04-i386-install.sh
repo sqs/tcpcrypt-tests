f4dfc33337c575332182eb6eea840fb989f64cc4
Quinn Slack <sqs@cs.stanford.edu>
2010-08-11 18:23:27 -0700

    improved test

--- ubuntu-10-04-i386-install.sh
+++ ubuntu-10-04-i386-install.sh
@@ -3,11 +3,29 @@ apt-get install -y \
 
 cd tcpcrypt/user
 make
+make install
 
-iptables -I INPUT -p tcp -j NFQUEUE --queue-num 666
-iptables -I OUTPUT -p tcp -j NFQUEUE --queue-num 666
+./launch_tcpcryptd.sh &
+sleep 0.5
 
-tcpcrypt/tcpcryptd -v &
-sleep 2
-LD_LIBRARY_PATH=`pwd`/lib test/tcpcrypt -v -t 0
-kill %1
+test/tcpcrypt -v -l 127.0.0.1 7777 > server.log &
+sleep 0.3
+test/tcpcrypt -v 127.0.0.1 7777 > client.log
+
+server_sessid=`grep 'Session ID' server.log`
+client_sessid=`grep 'Session ID' client.log`
+
+killall tcpcryptd tcpcrypt 2> /dev/null
+sleep 0.1
+
+echo -n Testing that local connection uses tcpcrypt and that session IDs match...
+if [ -n "$server_sessid" ] && \
+   [ -n "$client_sessid" ] && \
+   [ "$server_sessid" = "$client_sessid" ]
+then
+    echo OK
+    exit 0
+else
+    echo FAIL
+    exit 1
+fi
