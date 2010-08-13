9b7a9e0820b38a84a30d97db3494d0b6955a052e
Quinn Slack <sqs@cs.stanford.edu>
2010-08-12 21:15:04 -0700

    macosx port 80 and 7777 tests

--- macosx-106-i386-install.sh
+++ macosx-106-i386-install.sh
@@ -3,7 +3,16 @@ cd $HOME/tcpcrypt/user
 make
 
 sudo ./launch_tcpcryptd.sh &
-sleep 2
-LD_LIBRARY_PATH=`pwd`/lib test/tcpcrypt -v -t 0
-sudo killall tcpcryptd
-exit
+sleep 1.5
+
+echo Testing tcpcrypt connection to tcpcrypt.org:80...
+echo | test/tcpcrypt -vv 171.66.3.211 80
+R=$?
+
+echo Testing tcpcrypt connection on localhost:7777...
+test/tcpcrypt -v -l 127.0.0.1 7777 &
+test/tcpcrypt -v 127.0.0.1 7777
+R=$?
+
+sudo killall tcpcryptd tcpcrypt
+
